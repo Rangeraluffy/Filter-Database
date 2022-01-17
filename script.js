c32860d61d98b038b83042779c150d04ef16d2ea
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-17 11:06:15 +0100

    function utility order name

--- script.js
+++ script.js
@@ -6,9 +6,25 @@ async function getUsers(){
 
     const res = await fetch("https://randomuser.me/api/?nat=fr&results=50")
 
-    const results  = await res.json()
-    console.log(results);
+    const {results}  = await res.json()
+    
+    dataArray = orderList(results)
 
 }
 
 getUsers();
+
+function orderList(data){
+
+    const orderedData = data.sort((a,b) => {
+        if(a.name.last.toLowerCase() < b.name.last.toLowerCase()) {
+            return -1; 
+            }
+        if(a.name.last.toLowerCase() > b.name.last.toLowerCase()) {
+            return 1; 
+            }
+            return 0;
+        })
+
+    return orderedData;
+}
